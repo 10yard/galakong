48612f434f28b09954943114973b83330bee4f96
10yard <jon123wilson@hotmail.com>
2021-09-10 23:52:36 +0100

    Initial commit of a working version

---
+++ build_package.bat
@@ -0,0 +1,10 @@
+echo ----------------------------------------------------------------------------------------------
+echo  Package the dkshooter plugin 
+echo ----------------------------------------------------------------------------------------------
+copy readme.md dkshooter \ /Y
+
+echo **** package into a release ZIP getting the version from version.txt
+set /p version=<VERSION
+set zip_path="C:\Program Files\7-Zip\7z"
+del releases\dkshooter_plugin_%version%.zip
+%zip_path% a releases\dkshooter_plugin_%version%.zip dkshooter
